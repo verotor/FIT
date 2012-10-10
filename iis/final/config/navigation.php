fb0dac94020039a372d6e8ba8af2d1e1b5c7a387
dumblob <pacnerjan@volny.cz>
2012-10-10 22:00:54 +0200

    minor

--- iis/final/config/navigation.php
+++ iis/final/config/navigation.php
@@ -7,11 +7,23 @@
 		'aktuality'		=> array(
 			'page_name'		=> 'Aktuality',
 		),
-		'clanky'		=> array(
-			'page_name'		=> 'Průvodce Wellness'
+		//'clanky'		=> array(
+		//	'page_name'		=> 'Průvodce Wellness'
+		//),
+		'odkazy'		=> array(
+			'page_name'		=> 'Unknown00'
 		),
 		'odkazy'		=> array(
-			'page_name'		=> 'Odkazy'
+			'page_name'		=> 'Unknown01'
+		),
+		'odkazy'		=> array(
+			'page_name'		=> 'Unknown02'
+		),
+		'odkazy'		=> array(
+			'page_name'		=> 'Unknown03'
+		),
+		'odkazy'		=> array(
+			'page_name'		=> 'Unknown04'
 		),
 		'404'		=> array(
 			'page_name'		=> 'Stránka nenalezena'
