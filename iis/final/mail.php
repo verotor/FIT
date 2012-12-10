c80735d0658a34ecd48ed2e76037da8d20931e56
dumblob <pacnerjan@volny.cz>
2012-12-10 21:06:31 +0100

    mail notification script updated

--- iis/final/mail.php
+++ iis/final/mail.php
@@ -4,35 +4,46 @@ require_once 'classes/formparser/formparser.class.php';
 
 class MailInfo extends FormParser
 {
-  private $wtf;
-
   public function __construct()
   {
     parent::__construct('MailInfo');
   }
 
-  public function sendMail()
+  protected function validateData() {}
+  protected function readData() {}
+  protected function saveData() {}
+  protected function updateData() {}
+  protected function deleteData() {}
+
+  protected function createResult($admin) {}
+  protected function createResultOne($admin) {}
+  protected function createAdditionals($admin) {}
+
+  public function load() {}
+
+  public function sendMails()
   {
     if ($stmt = $this->dbc->query(
-      "SELECT reader.reader_email, title.title_title, title.title_subtitle ".
-      "FROM reader, reservation, title ".
-      "WHERE reservation.reader_id = reader.reader_id".
-      "  AND reservation.title_id = title.title_id".
-      "  AND title.title_copycountavail > 0"))
+      "SELECT reader.reader_email, title.title_title, title.title_subtitle "
+      ."FROM reader, reservation, title "
+      ."WHERE reservation.reader_id = reader.reader_id"
+      ."  AND reservation.title_id = title.title_id"
+      ."  AND title.title_copycountavail > 0"))
     {
       //$this->langs = array('cz' => array('name' => 'Čeština'));
 
-      foreach ($stmt as $item)
+      foreach ($stmt->fetch_all_array() as $item)
       {
-        $this->formdata = $stmt->fetch_row();
+        //$this->formdata = $stmt->fetch_row();
+        print_r($stmt->fetch_all_array());
 
-        if (! mail($item[0], "Title ".$item[1]." available",
-          "Title ".$item[1]." ".$item[2]." is available for borrowing."))
-        {
-          print "<h1>Sending mail to ".$item[0]." failed!</h1>";
-        }
-        //FIXME debug
-        else print "<h1>OK, email sent!</h1>";
+        //if (! mail($item[0], "Title ".$item[1]." available",
+        //  "Title ".$item[1]." ".$item[2]." is available for borrowing."))
+        //{
+        //  print "<h1>Sending mail to ".$item[0]." failed!</h1>";
+        //}
+        ////FIXME debug
+        //else print "<h1>OK, email sent!</h1>";
       }
     }
     else
@@ -42,16 +53,16 @@ class MailInfo extends FormParser
 
     /* 4 days before the borrow expires */
     if ($stmt = $this->dbc->query(
-      "SELECT reader.reader_email, title.title_title, title.title_subtitle ".
-      "FROM reader, borrow, copy, title ".
-      "WHERE borrow.reader_id = reader.reader_id".
-      "  AND borrow.copy_id = copy.copy_id".
-      "  AND copy.title_id = copy.title_id".
-      "  AND borrow.borrow_to = DATE_ADD(".date("Y-m-d").", INTERVAL 4 DAY"))
+      "SELECT reader.reader_email, title.title_title, title.title_subtitle "
+      ."FROM reader, borrow, copy, title "
+      ."WHERE borrow.reader_id = reader.reader_id"
+      ."  AND borrow.copy_id = copy.copy_id"
+      ."  AND copy.title_id = copy.title_id"
+      ."  AND borrow.borrow_to = DATE_ADD(".date("Y-m-d").", INTERVAL 4 DAY"))
     {
-      print "Hello, your borrow of "$item[1]." ".$item[2]." expires in 4 days.".
-      " Please return it back in time, otherwise you will penalized.";
-
+      //print "Hello, your borrow of "$item[1]." ".$item[2]." expires in 4 days.".
+      //" Please don't forget to return your borrowed title in time, otherwise you will penalized.";
+      print "ahoj";
     }
     else
     {
@@ -60,7 +71,7 @@ class MailInfo extends FormParser
   }
 }
 
-mailer = new MailInfo();
-mailer.sendMail();
+$mailer = new MailInfo();
+$mailer->sendMails();
 
 ?>
