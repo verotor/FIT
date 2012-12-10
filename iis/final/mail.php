96cc0e791c696b18a37f675a6847ff0963275596
dumblob <pacnerjan@volny.cz>
2012-12-10 22:18:41 +0100

    mail script done; documentation updated

--- iis/final/mail.php
+++ iis/final/mail.php
@@ -12,7 +12,6 @@ class MailInfo extends FormParser
   {
     parent::__construct('MailInfo');
     $this->setDBC(new DB_Connector());
-    //$this->dbc = ...
   }
 
   protected function validateData() {}
@@ -30,36 +29,31 @@ class MailInfo extends FormParser
   public function sendMails()
   {
     if ($stmt = $this->dbc->query(
-      //"SELECT reader.reader_email, title.title_title, title.title_subtitle "
-      //."FROM reader, reservation, title "
-      //."WHERE reservation.reader_id = reader.reader_id"
-      //."  AND reservation.title_id = title.title_id"
-      //."  AND title.title_copycountavail > 0"))
-      //"SELECT * "
-      //."FROM reader, reservation, title "
-      //."WHERE reservation.reader_id = reader.reader_id"
-      //."  AND reservation.title_id = title.title_id"))
-      "SELECT * FROM reader"))
+      "SELECT reader.reader_email, title.title_title, title.title_subtitle "
+      ."FROM reader, reservation, title "
+      ."WHERE reservation.reader_id = reader.reader_id"
+      ."  AND reservation.title_id = title.title_id"
+      ."  AND reader.reader_email IS NOT NULL"
+      ."  AND title.title_copycountavail > 0"))
     {
-      //$this->langs = array('cz' => array('name' => 'Čeština'));
-
       foreach ($stmt->fetch_all_array() as $item)
       {
-        //$this->formdata = $stmt->fetch_row();
-        print_r($stmt->fetch_all_array());
+        print "<p>Sending email to ".$item[0]." ...<br>";
 
-        //if (! mail($item[0], "Title ".$item[1]." available",
-        //  "Title ".$item[1]." ".$item[2]." is available for borrowing."))
-        //{
-        //  print "<h1>Sending mail to ".$item[0]." failed!</h1>";
-        //}
-        ////FIXME debug
-        //else print "<h1>OK, email sent!</h1>";
+        if (! mail($item[0], "Reserved title available for borrow [".$item[1]."]" ,
+          "Title ".$item[1]." ".$item[2]." is available for borrowing."))
+        {
+          print "Sending failed!</p>";
+        }
+        else
+        {
+          print "Email successfully sent.</p>";
+        }
       }
     }
     else
     {
-      print "<h1>No copies available (no mails send).</h1>";
+      print "<h3>No suitable reservations available (no mails send).</h3>";
     }
 
     /* 4 days before the borrow expires */
@@ -69,15 +63,28 @@ class MailInfo extends FormParser
       ."WHERE borrow.reader_id = reader.reader_id"
       ."  AND borrow.copy_id = copy.copy_id"
       ."  AND copy.title_id = copy.title_id"
+      ."  AND reader.reader_email IS NOT NULL"
       ."  AND borrow.borrow_to = DATE_ADD(".date("Y-m-d").", INTERVAL 4 DAY"))
     {
-      //print "Hello, your borrow of "$item[1]." ".$item[2]." expires in 4 days.".
-      //" Please don't forget to return your borrowed title in time, otherwise you will penalized.";
-      print "ahoj";
+      foreach ($stmt->fetch_all_array() as $item)
+      {
+        print "<p>Sending email to ".$item[0]." ...<br>";
+
+        if (! mail($item[0], "Your borrow expires in 4 days [".$item[1]."]",
+          "Your borrow of the title ".$item[1]." ".$item[2]." expires in 4 days.\n".
+          "Please do not forget to return the copy back in time to avoid penalization."))
+        {
+          print "Sending failed!</p>";
+        }
+        else
+        {
+          print "Email successfully sent.</p>";
+        }
+      }
     }
     else
     {
-      print "<h1>No copies available (no mails send).</h1>";
+      print "<h3>No borrows available (no mails send).</h3>";
     }
 
     $this->dbc->databaseReport();
