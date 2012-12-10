26645bcead75217af9530ac5f441c1501d913b1f
dumblob <pacnerjan@volny.cz>
2012-12-10 16:57:54 +0100

    mail notification script introduced

---
+++ iis/final/mail.php
@@ -0,0 +1,14 @@
+<?php
+
+// vytahnout z databaze ty, kteri maji za 4 dny rezervace
+// ???
+
+mail
+http://php.net/manual/en/function.mail.php
+if (! mail ( string $to , string $subject , string $message [, string $additional_headers [, string $additional_parameters ]] )
+)
+{
+  print "";
+}
+
+?>
